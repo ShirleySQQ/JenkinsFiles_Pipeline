da004d72fdf7616dfb950daa724e487f3ff47e30
ShirleySQQ <shirley_shi@epam.com>
2023-11-28 08:51:43 +0000

    New Feature Commit

--- test_assert.py
+++ test_assert.py
@@ -2,3 +2,4 @@ def test_assert():
     assert True == True, "True is not True"
     assert True == True, "True is not True"
     assert True == True, "True is not True"
+    assert True == True, "True is not True"
